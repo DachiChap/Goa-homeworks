06e9b15159a6ff6d7e1349ee609e3e73351c89ca
DachiChap <dachichapichadze02@gmail.com>
2024-06-05 19:50:58 +0400

    commit

--- level 3/classwork/classwork1.py
+++ level 3/classwork/classwork1.py
@@ -1,1 +1,36 @@
 print ("hello world")
+
+# = - მინიჭების ოპერატორი
+# toys = 5
+# trousers = 7\
+
+# int - integer
+# str - string
+# float - float
+#bool - boolean
+
+
+#variable - ცვლადი
+
+
+
+# int type variable
+number = 5
+
+#str type variable
+name = "Data"
+str = "My name is Dachi my age is 13"
+str = "13"
+
+#bool type variable
+is_right = True
+is_wrong = False
+
+#float type variable
+number2 = 10.5
+
+#concatenation - კონკატენაცია ანუ შეერთება
+str1 = "da"
+str2 = "ta"
+
+print(str1 +  str2)
