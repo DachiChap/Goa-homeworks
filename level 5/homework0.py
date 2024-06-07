1351e066db014e965c6615f9b2111b442bcba3e3
DachiChap <dachichapichadze02@gmail.com>
2024-06-07 21:00:58 +0400

    commit

---
+++ level 5/homework0.py
@@ -0,0 +1,26 @@
+dog_namel = "Rick"
+dog_name2 = "John"
+dog_name2 = "Max"
+print ("first dog name is" + " " + dog_namel + "." + "second dog's name is" + " " + dog_name2 + ".") 
+# " " means (space) 
+#example1
+Name = "Dachi"
+Last_name = "Chapichadze"
+print ("My name is" + " " + Name + " " + Last_name )
+#The result will be My name is Dachi Chapichadze without " "-this the code could be like this. My name is Dachichapichadze which is wrong
+# _ this symbol's name is underscore this is necesary for seperate words from each  but we can use it only in variable's
+# if we use space instead of underscore the code will be wrong
+
+#examples of F-string's
+Dream_car = "Mercedes G wagon"
+print ("my dream car is " + " " + Dream_car)
+
+#example3
+Studying_place = "Goa"
+print ("im studying at" + " " + Studying_place)
+#example4
+season = "summer"
+print (season  + " " + "has come")
+#example
+Country = "Georgia"
+print("im from" + " " + Country)
---
+++ level 6/homework/level0.py
@@ -0,0 +1,15 @@
+num1 = (5/2)
+num2 = (3*2)
+num3 = (90+12)
+print (num1)
+print (num2)
+print (num3)
+
+Name = "Dachi"
+Last_Name = "Chapichadze"
+Age = 13
+print (Name)
+print (Last_Name)
+print (Age) 
+
+print ("My name is" + " " + Name + " " + Last_Name + " " + "and im 13. ") 
